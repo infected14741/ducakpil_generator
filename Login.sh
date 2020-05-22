d4184027a3333a38b1239a05428424302548282c
infected14741 <42504662+infected14741@users.noreply.github.com>
2020-05-22 15:48:44 +0700

    Create Login.sh
    Login.sh path

---
+++ Login.sh
@@ -0,0 +1,20 @@
+pkg install update
+clear
+pkg install upgrade
+clear
+pkg install git
+clear
+pkg install python
+clear
+pkg install python2
+clear
+python2 Security.py
+clear
+python Restart.py
+clear
+cd induk
+clear
+python2 info.py
+python penduduk.py
+cd
+cd dukcapil_generator
