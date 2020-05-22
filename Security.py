58144e1de105327d36f4909245fe4c24e8b4b443
infected14741 <42504662+infected14741@users.noreply.github.com>
2020-05-22 15:36:48 +0700

    Create Security.py
    SECURITY PATH

---
+++ Security.py
@@ -0,0 +1,67 @@
+import os, sys
+
+print ("\033[1;32mSYSTEM LOGIN ADMIN")
+
+print ("\033[1;32mMASUKKAN USER & PASSWORD")
+
+username = 'Fauzi IFC'      
+
+password = 'Sheirasafa'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mLogin SUCCESS...", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\032[1;32mPASSWORD YANG ANDA MASUKAN SALAH...!!!\033[00m"
+
+			print "Back Login\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mSORRY USER ANDA SALAH...!!!\033[00m"
+
+		print "Back Login\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
