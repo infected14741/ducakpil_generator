56e3ed648df7fe5943a3213dc40a5487b35dbec9
infected14741 <42504662+infected14741@users.noreply.github.com>
2020-05-22 16:02:04 +0700

    Create info.py
    INFO.py Path

---
+++ induk/info.py
@@ -0,0 +1,67 @@
+import os, sys
+
+print ("\033[1;32mSYSTEM LOGIN INFORMASI")
+
+print ("\033[1;32mMASUKKAN USER & PASSWORD")
+
+KK = 'KARTU KELUARGA'      
+
+NIK= 'NOMER INDUK KEPENDUDUKAN'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mLogin SUCCESS...", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\032[1;32mKK YANG ANDA MASUKAN SALAH...!!!\033[00m"
+
+			print "Back Login\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mNIK YANG ANDA MASUKAN SALAH...!!!\033[00m"
+
+		print "Back Login\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
